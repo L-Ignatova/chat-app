3a626efb5d132de358f0cf0af449edb4eb64ac2f
Ignatova, Liliya <12227+liliya-ignatova@users.noreply.eos2git.cec.lab.emc.com>
2022-11-10 11:47:05 +0200

    add constants, styles, faker

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+const SEND_MESSAGE = "send_message";
+const RECEIVE_MESSAGE = "receive_message";
+const USER_STARTED_TYPING = "user_started_typing";
+const USER_IS_TYPING = "user_is_typing";
+const USER_STOPPED_TYPING = "user_stopped_typing";
+const USER_IS_NOT_TYPING = "user_is_not_typing";
+const ON_CONNECTION = "connection";
+
+module.exports = {
+  SEND_MESSAGE,
+  RECEIVE_MESSAGE,
+  USER_STARTED_TYPING,
+  USER_STOPPED_TYPING,
+  USER_IS_TYPING,
+  USER_IS_NOT_TYPING,
+  ON_CONNECTION
+}
