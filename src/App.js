01746ccb21fd794ac1761698d543b34d7e569340
Liliya Ignatova <liliya.oignatova@gmail.com>
2022-11-14 11:00:03 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import io from "socket.io-client";
-import ChatControls from './ChatControls';
-import ChatRoom from './ChatRoom';
-import LoginScreen from './LoginScreen';
+import ChatControls from './components/ChatControls';
+import ChatRoom from './components/ChatRoom';
+import LoginScreen from './components/LoginScreen';
+import { SocketContext, socket } from './context/socket';
+import { USER_HAS_JOINED } from './utils/constants';
 
-const socket = io.connect("http://localhost:3001");
+import bgTranslation from "./utils/languages/bg.json"
+import enTranslation from "./utils/languages/en.json"
+const locale = {
+  "en": enTranslation,
+  "bg": bgTranslation,
+};
 
 const App = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [username, setUsername] = useState("");
+  const [language, setLanguage] = useState("en");
+  const [activeLocale, setActiveLocale] = useState(locale.en);
+
+  useEffect(() => {
+    setActiveLocale(locale[language]);
+  }, [language]);
+
+  useEffect(() => {
+    socket.emit(USER_HAS_JOINED, username);
+  }, [isLogged]);
 
   return (
-    <div className='App'>
-      {isLogged ? <>
-          <ChatRoom socket={socket}/>
-          <ChatControls username={username} socket={socket}/>
-        </> : <LoginScreen 
-          setIsLogged={setIsLogged} 
-          username={username} 
-          setUsername={setUsername}
-        />}
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className='App'>
+        <div className='languages-buttons'>
+          <button onClick={() => setLanguage("bg")}>bg</button>
+          <button onClick={() => setLanguage("en")}>en</button>
+        </div>
+        {isLogged ? <>
+            <ChatRoom username={username} activeLocale={activeLocale}/>
+            <ChatControls username={username} activeLocale={activeLocale}/>
+          </> : <LoginScreen
+            setIsLogged={setIsLogged}
+            username={username}
+            setUsername={setUsername}
+            activeLocale={activeLocale}
+          />}
+      </div>
+    </SocketContext.Provider>
   );
 }
 
