dadaff20bf84124a3fc176d8dbc90cb82099a9a7
Liliya Ignatova <liliya.oignatova@gmail.com>
2022-11-17 12:43:58 +0200

    add LanguageControls component

---
+++ src/components/LanguageControls.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../utils/locales/config"
+import i18n from '../utils/locales/config';
+
+const LanguageControls = () => {
+  const handleChangeLanguage = (ev) => {
+    i18n.changeLanguage(ev.target.innerHTML);
+  };
+ 
+  return (
+    <div className='round-buttons'>
+      <button onClick={handleChangeLanguage}>bg</button>
+      <button onClick={handleChangeLanguage}>en</button>
+    </div>
+  );
+}
+
+export default LanguageControls;
