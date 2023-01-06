b8508fa154647f97c08cda9b53b88310eaadf562
Liliya Ignatova <liliya.oignatova@gmail.com>
2023-01-06 15:20:39 +0200

    Add scrollToBottom to UserTyping

--- src/components/UserTyping.js
+++ src/components/UserTyping.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { scrollToChatBottom } from "../utils/chatUtils";
 import { ChatRoom_UserIsTypingMsg } from "../utils/locales/translationKeys";
 
 const UserTyping = ({ show }) => {
   const { t } = useTranslation();
+  
+  useEffect(() => {
+    scrollToChatBottom();
+  });
 
   return (
     <p className='user-typing'>
