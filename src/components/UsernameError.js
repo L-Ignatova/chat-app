d8c2bd6285a3d32e83c87fe4d8a7e724dab66558
Liliya Ignatova <liliya.oignatova@gmail.com>
2023-01-09 13:38:36 +0200

    Create UsernameError.js

---
+++ src/components/UsernameError.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UsernameErrorMsg = () => {
+  return (
+    <p className="error-msg">Username must be at least 4 characters long.</p>
+  );
+}
+
+export default UsernameErrorMsg;
