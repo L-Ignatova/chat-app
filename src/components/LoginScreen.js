7d144c5ad11d0b1c900aee338859b7fa88005b7a
Liliya Ignatova <liliya.oignatova@gmail.com>
2023-01-09 13:42:10 +0200

    Update LoginScreen.js

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,19 +1,36 @@
 import React, { useContext, useState } from 'react';
 import "../utils/locales/config"
 import {useTranslation} from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import { ChatControls_LoginButton, ChatControls_UsernamePlaceholder } from "../utils/locales/translationKeys";
 import { SocketContext } from '../context/socket';
 import { USER_HAS_JOINED } from '../utils/constants';
+import UsernameErrorMsg from './UsernameError';
+import { isLongerThan } from '../utils/chatUtils';
 
 const LoginScreen = ({ setIsLogged, setUsernameContext }) => {
   const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState(false);
   const { t } = useTranslation();
   const socket = useContext(SocketContext);
+  const navigate = useNavigate();
 
   const handleLogin = () => {
+    if (!isLongerThan(username)) {
+      setUsernameError(true);
+      return;
+    }
     setUsernameContext(username);
     setIsLogged(true);
+    navigate("/chat");
     socket.emit(USER_HAS_JOINED, username);
+  };
+
+  const onInputChange = (ev) => {
+    setUsername(ev.target.value);
+    if (usernameError && isLongerThan(ev.target.value)) {
+      setUsernameError(false);
+    };
   };
  
   return (
@@ -22,8 +39,9 @@ const LoginScreen = ({ setIsLogged, setUsernameContext }) => {
         className='input-username'
         placeholder={t(ChatControls_UsernamePlaceholder)}
         value={username}
-        onChange={(ev) => setUsername(ev.target.value)}
+        onChange={onInputChange}
       />
+      {usernameError && <UsernameErrorMsg />}
       <button onClick={handleLogin}>{t(ChatControls_LoginButton)}</button>
     </div>
   );
